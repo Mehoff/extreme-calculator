584a5415d8660e51451fd04b6f0b5cb338d3a7b7
itmaxxx <dmitrievmaks77@gmail.com>
2021-09-13 11:28:05 +0300

    Layout changes

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-console.log(sum(5, 10));
+let resultValue = document.getElementById('resultValue');
+let firstNumber = document.getElementById('firstNumber');
+let secondNumber = document.getElementById('secondNumber');
+
+function showValidationErrorWithMessage(errorBlockName, msg = 'Error') {
+    let error = document.getElementById(errorBlockName);
+    error.style.display = 'block';
+    error.innerText = msg;
+}
+
+function hideValidationError(errorBlockName) {
+    let error = document.getElementById(errorBlockName);
+    error.style.display = 'none';
+}
+
+function handleCalculate() {
+    showValidationErrorWithMessage('firstNumberError', 'What is it');
+    showValidationErrorWithMessage('secondNumberError', 'What is it');
+
+    resultValue.innerText = sum(firstNumber, secondNumber);
+}
