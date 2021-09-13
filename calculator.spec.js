fe02990b40e631a2c72524ac18930ffbc76d54a2
offiza <matveygorelik070118@gmail.com>
2021-09-13 12:47:14 +0300

    Added tests

--- calculator.spec.js
+++ calculator.spec.js
@@ -3,9 +3,30 @@ const { expect, test, describe } = require("@jest/globals");
 
 describe("Calculator functions", () => {
     test("adds 2 + 3 to be equal 5", () => {
-        expect(sum(2, 3)).toEqual(5);
+        expect(sum(2, 3)).toEqual({ answer: 5 });
     });
     test("adds V + V to be equal X", () => {
-        expext(sum("V", "V")).toEqual("X")
+        expext(sum("V", "V")).toEqual({ answer: "X" })
+    });
+    test("adds IX + XL to be equal LIV", () => {
+        expext(sum("IX", "XLI")).toEqual({ answer: "LIV" })
+    });
+    test("return error when we pass XXXX + XL", () => {
+        expext(sum("XXXX", "XL")).toEqual({ error: "" })
+    });
+    test("return error when we pass IM + I", () => {
+        expext(sum("IM", "I")).toEqual({ error: "" })
+    });
+    test("adds 9999999 + 1111 to be equal LIV", () => {
+        expext(sum("9999999", "1111")).toEqual({ answer: "10001110" })
+    });
+    test("return error when we pass XX + 11", () => {
+        expext(sum("XX", "11")).toEqual({ error: "" })
+    });
+    test("return error when we pass Y + X", () => {
+        expext(sum("XX", "11")).toEqual({ error: "" })
+    });
+    test("return error when we pass Y + X", () => {
+        expext(sum("XX", "11")).toEqual({ error: "" })
     });
 });
