de541965b8ced03bce36cf5812eb67a60a72ee43
offiza <matveygorelik070118@gmail.com>
2021-09-13 10:50:49 +0300

    Added function arabicToRoman

--- calculator.js
+++ calculator.js
@@ -44,4 +44,29 @@ function char_to_int(character) {
     }
 }
 
-console.log(romanToArabic('IX'));
+function arabicToRoman(arabic) {
+    var roman = {
+        M: 1000,
+        CM: 900,
+        D: 500,
+        CD: 400,
+        C: 100,
+        XC: 90,
+        L: 50,
+        XL: 40,
+        X: 10,
+        IX: 9,
+        V: 5,
+        IV: 4,
+        I: 1
+    };
+    var str = '';
+
+    for (var i of Object.keys(roman)) {
+        var q = Math.floor(arabic / roman[i]);
+        arabic -= q * roman[i];
+        str += i.repeat(q);
+    }
+
+    return str;
+}
